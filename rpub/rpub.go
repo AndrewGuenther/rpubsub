853c0379ea280475da779beffdc3c1b9c43f1dd4
Andrew Guenther <guenther.andrew.j@gmail.com>
2015-08-07 00:24:10 -0700

    Implemented most of rpub logic

--- rpub/rpub.go
+++ rpub/rpub.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"io"
 	"log"
+	"os"
 
 	"github.com/garyburd/redigo/redis"
 )
@@ -12,4 +15,19 @@ func main() {
 		log.Fatal(err)
 	}
 	defer c.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+
+		c.Do("PUBLISH", os.Args[1], line)
+	}
 }
