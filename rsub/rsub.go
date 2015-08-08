9da3f4bce7e8bc1a6c55aedece579dc389a0618a
Andrew Guenther <guenther.andrew.j@gmail.com>
2015-08-07 18:38:13 -0700

    Implemented rsub

--- rsub/rsub.go
+++ rsub/rsub.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/AndrewGuenther/rpubsub"
+	"github.com/garyburd/redigo/redis"
 )
 
 func main() {
 	c := rpubsub.BuildConn()
 	defer c.Close()
+
+	psc := redis.PubSubConn{c}
+	psc.Subscribe(os.Args[1])
+	for {
+		switch v := psc.Receive().(type) {
+		case redis.Message:
+			fmt.Printf("%s\n", v.Data)
+		case redis.Subscription:
+			continue
+		case error:
+			return
+		}
+	}
 }
